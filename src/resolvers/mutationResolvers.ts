c335517cd7a07eedcee599a7474aa28c84ed3cd7
Daniel Redly <dredly@gmail.com>
2022-09-18 14:20:48 +0100

    Add create game mutation

--- src/resolvers/mutationResolvers.ts
+++ src/resolvers/mutationResolvers.ts
@@ -1,6 +1,7 @@
 import { state } from './resolvers';
 import { createPlayer as createPlayerAction } from '../actions';
 import { createLobby as createLobbyAction } from '../actions';
+import { createGame as createGameAction } from '../actions';
 import { LobbyOptions } from '../types';
 
 const mutationResolvers = {
@@ -8,7 +9,8 @@ const mutationResolvers = {
 		state.lobby = createLobbyAction(args.lobbyOptions);
 		return state.lobby;
 	},
-	createPlayer: (_root: undefined, args: {name: string}) => createPlayerAction(args.name, state.lobby)
+	createPlayer: (_root: undefined, args: {name: string}) => createPlayerAction(args.name, state.lobby),
+	createGame: (_root: undefined, args: {playerID: string}) => createGameAction(args.playerID, state.lobby)
 };
 
 export default mutationResolvers;
